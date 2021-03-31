aa1bfd29f82c1b9655e7b8680c7e4849c1925f86
James Sherry <james.m.sherry@googlemail.com>
2021-03-31 20:40:58 +0100

    minor change

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <div className="page">
       <Header />
       <PageFrame>
-        <h1>Home</h1>
+        <h1>Home change!!!!</h1>
         <ProductsList />
       </PageFrame>
     </div>
